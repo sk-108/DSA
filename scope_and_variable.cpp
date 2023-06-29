012bddb1aed686ef1115597fd3a3914692981aba
sk-108 <souravkumar31cse@gmail.com>
2023-06-29 15:57:37 +0530

    operator and scope

---
+++ scope_and_variable.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int b; // prints 0 value
+
+int main()
+{
+   int a; // prints garbage value
+   cout<<a<<endl;
+   cout<<b<<endl;
+    
+}
---
+++ shift_operator.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    cout<<(3<<3)<<endl;
+    cout<<(8>>2)<<endl;
+    
+}
